4e2262498786809ebc487a4f87deecce1b9f0937
Aleksanteri Hämäläinen <aleksanterih@gmail.com>
2015-11-15 17:44:07 +0200

    Generator and parameters started
    Moved generation into its own file and made a new file for parameters.

--- constants.py
+++ constants.py
@@ -1,11 +1,33 @@
+## General ##
+
+# FIXME values
 TOPICS = ["A1", "A2", "B", "C", "D"]
 
 # Degree of interest
 # Very interested, interested, mildly interested, not interested
-DoI = [0.8, 0.5, 0.3, 0]
+# FIXME values
+DOI = [0.8, 0.5, 0.3, 0]
+
+# FIXME value
+TIMESTEPS = 100
+
+## People ##
+
+# Amount of people in the system
+PEOPLE = 10
 
 # Probabilities of sharing and creating posts
+# FIXME values
 PSHARE = 0.2
 PCREATE = 0.1
 
-TIMESTEPS = 100
+## Sites ##
+
+# Site post creation variables
+# FIXME values
+MEAN = 20
+DEVIATION = 3
+
+
+## Filter ##
+FILTERMAX = 50
--- main.py
+++ main.py
@@ -1,64 +1,11 @@
-import random
-
 # Constants
-from constants import *
-# import generator
-# Classes
-from person import Person
-from filter import Filter
-from sites import Site
+from constants import TIMESTEPS
+from generator import  generate
 
-TIMESTEPS = 100
-
-### FIXME Temporary generation process
-
-### FIXME Topics
-topics = ["A1", "A2", "B", "C", "D"]
-
-sites = []
-
-for topic in topics:
-	# FIXME mean and deviation
-	sites.append(Site(topic, 20, 5))
-
-## FIXME Current Degree of Interest (DoI)
-DoI = [0.8, 0.5, 0.3, 0]
-
-### Filter
-filter = Filter(50)
-
-### People
-people = []
-
-for ii in range(10):
-
-	## Interests
-	interests = {}
-	topicsCopy = topics[:]
-	# Very interested
-	veryInterested = random.choice(topicsCopy)
-	topicsCopy.remove(veryInterested)
-	interests[veryInterested] = DoI[0]
-	# Interested and mildly interested
-	interest1 = random.choice(topicsCopy)
-	topicsCopy.remove(interest1)
-	interest2 = random.choice(topicsCopy)
-	topicsCopy.remove(interest2)
-	interests[interest1] = random.choice([DoI[1], DoI[2]])
-	interests[interest2] = random.choice([DoI[1], DoI[2]])
-	# The rest (not interested)
-	for topic in topicsCopy:
-		interests[topic] = 0
-
-	## Friends
-	friends = []
-
-	## Create
-	people.append(Person(ii, interests, friends, 0.2, 0.1, DoI))
-
-### END of temporary generation process
+sites, people, filter = generate()
 
 ##### Main loop #####
+
 for ii in range(TIMESTEPS):
 	# Create posts
 	for site in sites:
@@ -79,6 +26,5 @@ for ii in range(TIMESTEPS):
 		person.create()
 		person.buffer = []
 
-### END
-
+# FIXME result processing
 print people[0].received
